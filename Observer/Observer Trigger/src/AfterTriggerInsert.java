2e88c4b3c5f05ac41057be6f99cecf97c3927597
Vasia <fgzf.fgzf.1999@mail.ru>
2018-11-16 15:52:45 +0200

    Add Observer Trigger

---
+++ Observer/Observer Trigger/src/AfterTriggerInsert.java
@@ -0,0 +1,32 @@
+/**
+ * @author Zaichenko Vasyl
+ * Клас підписник AfterTrigger який реалізує інтерфейс Trigger
+ */
+public class AfterTriggerInsert implements Trigger {
+    private int triggerValue;
+    /**
+     * Назва триггера
+     */
+    private String nameTrigger;
+
+    /**
+     * Конструктор AfterTrigger
+     *
+     * @param nameTrigger - назва триггера
+     */
+
+    public AfterTriggerInsert(int triggerValue, String nameTrigger) {
+        this.triggerValue = triggerValue;
+        this.nameTrigger = nameTrigger;
+    }
+
+    /**
+     * Метод реагування підписника
+     */
+    @Override
+    public void handleEvent(int value) throws Exception {
+        if(value >=0 && value<= triggerValue)
+            System.out.println("Insert Trigger "+nameTrigger+" After: value correct");
+        else throw new Exception("Value Incorrect");
+    }
+}
