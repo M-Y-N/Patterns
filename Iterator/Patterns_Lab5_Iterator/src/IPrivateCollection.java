3340e327a6831cf198a037d4aa286ed9d91205ab
Ivan Zaichenko <i.v.zaichenko70@gmail.com>
2018-11-16 15:23:31 +0200

    Add pattern Iterator

---
+++ Iterator/Patterns_Lab5_Iterator/src/IPrivateCollection.java
@@ -0,0 +1,14 @@
+/**
+ * @author Ivan Zaichenko
+ * @version 1.0
+ * Інтерфейс IPrivateCollection, з методом getIterator свого ітератора
+ */
+
+public interface IPrivateCollection {
+    /**
+     * Отримання ітератору
+     *
+     * @return повертає власний ітератор
+     */
+    Iteratorable getIterator();
+}
