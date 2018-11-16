78c43aec0ef6ea98aa75e39b68f58f86b2a9d3af
Ivan Zaichenko <i.v.zaichenko70@gmail.com>
2018-11-16 15:21:44 +0200

    Add pattern Composite

---
+++ Composite/Lab3_Patterns/src/BaseShape.java
@@ -0,0 +1,73 @@
+/**
+ * @author Ivan Zaichenko
+ * @version 1.0
+ * Абстрактний клас BaseShape який реалізує інтерфейс Shape
+ */
+abstract class BaseShape {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    /**
+     * Конструктор BaseShape з параметрами
+     *
+     * @param x      величина Х
+     * @param y      величина У
+     * @param width  ширина
+     * @param height довжина
+     */
+    BaseShape(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+    }
+
+    /**
+     * Гетер для Х
+     *
+     * @return повертає Х
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Гетер для Y
+     *
+     * @return повертає Y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Гетер для width
+     *
+     * @return повертає width
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * Гетер для height
+     *
+     * @return повертає height
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * Вивід на екран
+     */
+    public void paint() {
+
+    }
+
+    public abstract CompoundShape add(BaseShape component);
+
+}
---
+++ Composite/Lab3_Patterns/src/Shape.java
@@ -0,0 +1,18 @@
+///**
+// * @author Ivan Zaichenko
+// * @version 1.0
+// * Інтерфейс Shape
+// * Загальний інтерфейс компонентів.
+// */
+//public interface Shape {
+//    public int getX();
+//
+//    public int getY();
+//
+//    public int getWidth();
+//
+//    public int getHeight();
+//
+//    public void paint();
+//}
+
